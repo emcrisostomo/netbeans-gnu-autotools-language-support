9b6a397424cb75d8c3d648339e6deb94b64c2cda
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-03-05 11:51:44 +0100

    Organize classes in packages according to their functional purpose.

--- src/org/netbeans/gnu/autotools/autoconf/syntax/ACSyntaxErrorHighlightingTask.java
+++ src/org/netbeans/gnu/autotools/autoconf/syntax/ACSyntaxErrorHighlightingTask.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.netbeans.gnu.autotools.autoconf.parser;
+package org.netbeans.gnu.autotools.autoconf.syntax;
 
 import java.util.ArrayList;
 import java.util.List;

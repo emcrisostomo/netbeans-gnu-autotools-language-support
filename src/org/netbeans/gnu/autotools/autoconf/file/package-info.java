490bd8077d84a2081db7bce53318596c7ae27959
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-03-09 11:46:23 +0100

    Register Autoconf template in "Autotools" folder.

--- src/org/netbeans/gnu/autotools/autoconf/file/package-info.java
+++ src/org/netbeans/gnu/autotools/autoconf/file/package-info.java
@@ -14,7 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-@TemplateRegistration(folder = "Other", content = "ACTemplate.ac", displayName = "Autoconf file")
+@TemplateRegistration(
+        folder = "Autotools",
+        content = "ACTemplate.ac",
+        displayName = "Autoconf file")
 package org.netbeans.gnu.autotools.autoconf.file;
 
 import org.netbeans.api.templates.TemplateRegistration;

ada5143683d34f39ac29cba1871ed8ee8174953f
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-03-12 21:33:56 +0100

    Implement code completion for Autoconf files.

---
+++ src/org/netbeans/gnu/autotools/autoconf/completion/AutoconfCompletionDocumentation.java
@@ -0,0 +1,54 @@
+/* 
+ * Copyright (C) 2014, Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.netbeans.gnu.autotools.autoconf.completion;
+
+import java.net.URL;
+import javax.swing.Action;
+import org.netbeans.spi.editor.completion.CompletionDocumentation;
+
+/**
+ *
+ * @author Enrico M. Crisostomo
+ */
+class AutoconfCompletionDocumentation implements CompletionDocumentation {
+
+    private final AutoconfCompletionItem item;
+
+    public AutoconfCompletionDocumentation(AutoconfCompletionItem item) {
+        this.item = item;
+    }
+
+    @Override
+    public String getText() {
+        return "Information about " + item.text;
+    }
+
+    @Override
+    public URL getURL() {
+        return null;
+    }
+
+    @Override
+    public CompletionDocumentation resolveLink(String string) {
+        return null;
+    }
+
+    @Override
+    public Action getGotoSourceAction() {
+        return null;
+    }
+}
